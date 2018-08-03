7f5e896a757b90545c748bdaf695911f8d32fef5
Okwe <Okweai@gmail.com>
2018-08-04 00:34:48 +0100

    init

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace myDonet.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
